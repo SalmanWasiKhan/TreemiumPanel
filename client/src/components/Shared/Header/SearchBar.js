3cd242781104cd76dd343e14d9a5bd41c6999b60
Salman Wasi <salmanwasi8@gmail.com>
2022-05-07 20:10:13 +0500

    Header Responsive

--- client/src/components/Shared/Header/SearchBar.js
+++ client/src/components/Shared/Header/SearchBar.js
@@ -8,7 +8,7 @@ const SearchBar = () => {
 
   return (
     <form
-      className="relative flex w-96 overflow-hidden rounded-md border border-border bg-body-bg transition focus-within:border-primary"
+      className="relative flex w-full overflow-hidden rounded-md border border-border bg-body-bg transition focus-within:border-primary "
       onSubmit={(e) => {
         e.preventDefault();
         setSearchParam('q', value);
@@ -16,14 +16,14 @@ const SearchBar = () => {
     >
       <input
         type="text"
-        className="flex-1 border-0 bg-transparent py-2.5 px-5 font-medium text-heading placeholder:font-medium placeholder:text-heading focus:ring-0"
+        className="min-w-[40px] flex-1 flex-shrink border-0 bg-transparent py-2.5 px-5 font-medium text-heading placeholder:font-medium placeholder:text-heading focus:ring-0"
         placeholder="Search"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
       <button
         type="submit"
-        className="flex items-center justify-center bg-primary px-3 text-white "
+        className="flex flex-shrink-0 items-center justify-center bg-primary px-3 text-white "
       >
         <SearchIcon className="h-6 w-6" />
       </button>
--- client/src/components/Shared/Header/index.js
+++ client/src/components/Shared/Header/index.js
@@ -1,11 +1,17 @@
 import MainMenu from './MainMenu';
 import SearchBar from './SearchBar';
+import logo from '../../../assets/images/logo.png';
 
 const Header = () => {
   return (
     <header className="bg-body-bg py-2.5">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-4">
-        <SearchBar />
+        <div className="flex flex-1 items-center gap-4">
+          <img src={logo} alt="Treemium" className="block w-7 md:hidden" />
+          <div className="mr-4 w-44 sm:w-auto sm:flex-1 md:w-96 md:flex-grow-0">
+            <SearchBar />
+          </div>
+        </div>
         <MainMenu />
       </div>
     </header>
